789eebda7a9fb2153a3eabd9bba22977de9d855a
xxf1995 <xiax@carleton.edu>
2018-02-06 03:35:24 -0600

    [feat] prelimary work on scrooge assignment

--- Princeton Course/Assignments/Assignment_1 Scrooge Coin/src/TxHandler.java
+++ Princeton Course/Assignments/Assignment_1 Scrooge Coin/src/TxHandler.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CyclicBarrier;
 
 public class TxHandler {
 
@@ -23,12 +26,38 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
+
         ArrayList<Transaction.Output> outputs = tx.getOutputs();
         ArrayList<Transaction.Input> inputs = tx.getInputs();
+        ArrayList<UTXO> allUTXO = this.uPool.getAllUTXO();
+        ArrayList<Transaction.Output> poolOutputs = new ArrayList<Transaction.Output>();
 
-        for (int i = 0; i< outputs.size(); i++) {
-            if
+        ArrayList<UTXO> checkDup = new ArrayList<UTXO>();
+        for (UTXO utxo:allUTXO) {
+            poolOutputs.add(this.uPool.getTxOutput(utxo));
         }
+
+        for (int i = 0; i < outputs.size(); i++) {
+            //(1) all outputs claimed by {@code tx} are in the current UTXO pool
+            if (!outputs.get(i).equals(poolOutputs.get(i)))
+                return false;
+
+            //(2) the signatures on each input of {@code tx} are valid
+            if (!Crypto.verifySignature(outputs.get(i).address, tx.getRawTx(), inputs.get(i).signature))
+                return false;
+
+            if (outputs.get(i).value < 0)
+                return false;
+            checkDup.add(new UTXO(inputs.get(i).prevTxHash, inputs.get(i).outputIndex));
+        }
+        Set<UTXO> checkDupset = new HashSet<UTXO>(checkDup);
+        if (checkDupset.size() < checkDup.size())
+            return false;
+
+        for  (int j = 0; j < outputs.size(); j++) {
+            if (Crypto.verifySignature(input.signature))
+        }
+
     }
 
     /**
